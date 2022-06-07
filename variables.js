49a01cdbefe5c206a60816991afdc123d739963b
Nathaniel Macapinlac <83597466+nasan016@users.noreply.github.com>
2022-06-07 01:46:49 -0400

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,16 @@
+const github = "https://github.com/nasan016"
+const linkedin = "https://www.linkedin.com/in/nathanielmacapinlac/"
+
+help = "<br>&nbsp;&nbsp;<span class=\"command\">'about'</span>&nbsp;&nbsp;&nbsp;<span class=\"white\">&nbsp;&nbsp;who made this site?<br>&nbsp;&nbsp;<span class=\"command\">'whoami'</span>&nbsp&nbsp;&nbsp what's a nasan?<br>&nbsp&nbsp<span class=\"command\">'secret'</span> &nbsp&nbsp ?<br>&nbsp&nbsp<span class=\"command\">'banner'</span> &nbsp&nbsp display header<br>&nbsp&nbsp<span class=\"command\">'clear'</span> &nbsp&nbsp&nbsp clears the terminal<br><br>";
+
+about = "<br>Hi, I'm Nathaniel Macapinlac but most people know me as Nathan.<br>I'm a student studying Computer Engineering and Computer Science at NYU Tandon.<br><br>";
+
+secret = "It's a secret ... even I don't know it.<br>I think there might be a password somewhere...<br><br>";
+
+errorMsg = "COMMAND NOT FOUND<br><br>";
+
+whoami = '<br>&nbsp;&nbsp;github &nbsp;&nbsp;&nbsp;&nbsp;<a href="' + github + '" target="_blank">github/nasan016</a><br>&nbsp;&nbsp;linkedin &nbsp;&nbsp;<a href="' + linkedin + '" target="_blank">linkedin/nathanielmacapinlac</a><br><br>';
+
+password = "Sorry, there isn't any secret yet..."
+
+banner =  "<pre> _________  ________  ________  ________  _________<br>|\\   ___  \\|\\   __  \\|\\   ____\\|\\   __  \\|\\   ___  \\ <br>\\ \\  \\\\ \\  \\ \\  \\|\\  \\ \\  \\___|\\ \\  \\|\\  \\ \\  \\\\ \\  \\<br> \\ \\  \\\\ \\  \\ \\   __  \\ \\_____  \\ \\   __  \\ \\  \\\\ \\  \\<br>  \\ \\  \\\\ \\  \\ \\  \\ \\  \\|____|\\  \\ \\  \\ \\  \\ \\  \\\\ \\  \\<br>   \\ \\__\\\\ \\__\\ \\__\\ \\__\\____\\_\\  \\ \\__\\ \\__\\ \\__\\\\ \\__\\<br>    \\|__| \\|__|\\|__|\\|__|\\_________\\|__|\\|__|\\|__| \\|__|<br>                        \\|_________|</pre><p>Welcome to NasanTerminal v0.0.1<br>Type <span class=\"command\">'help'</span> for a list of commands.</p>"
