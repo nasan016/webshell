f9a68db32720ccc10839736c4bffcbb805c3d84f
Nathaniel Macapinlac <nlm040603@gmail.com>
2023-12-02 16:27:30 -0500

    rewriting command handler

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const TERMINAL = document.getElementById("terminal")!;
+const USERINPUT = document.getElementById("user-input") as HTMLInputElement;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let WRITELINES = document.getElementById("write-lines")!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let isPassword = false;
+let isCorrect = false;
+let userInput = "";
+
+function userInputHandler(e : KeyboardEvent) {
+  const key = e.code;
+
+  if(key === "Enter") {
+    console.log(USERINPUT.value);
+    USERINPUT.value = ""
+  };
+};
