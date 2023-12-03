882dc1e4dee3c3ad8d522ae2dbac3ec0f96683c2
Nathaniel Macapinlac <nlm040603@gmail.com>
2023-12-03 02:58:33 -0500

    i accidentally rm -rf the whole src folder

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,50 @@
 const TERMINAL = document.getElementById("terminal")!;
 const USERINPUT = document.getElementById("user-input") as HTMLInputElement;
+const PROMPT = document.getElementById("prompt")!;
 
 let WRITELINES = document.getElementById("write-lines")!;
+let userInput : string;
 
-let isPassword = false;
-let isCorrect = false;
-let userInput = "";
+//WRITELINESCOPY is used to during the "clear" command
+const WRITELINESCOPY = WRITELINES
+
+const FOCUS = () => {
+  USERINPUT.focus();
+}
+
+const SCROLLTOBOTTOM = () => {
+  const MAIN = document.getElementById("main")!;
+  MAIN.scrollTop = MAIN.scrollHeight;
+}
 
 function userInputHandler(e : KeyboardEvent) {
   const key = e.code;
 
-  if(key === "Enter") {
-    console.log(USERINPUT.value);
-    USERINPUT.value = ""
-  };
-};
+  switch(key) {
+    case "Enter":
+      enterKey();
+      SCROLLTOBOTTOM();
+      break;
+  }
+}
+
+function enterKey() {
+  const resetInput = "";
+  userInput = USERINPUT.value;
+  userInput = userInput.trim();
+
+  const p = document.createElement("p");
+  p.innerHTML = PROMPT.innerHTML.concat(" ", userInput);  
+  WRITELINES.parentNode!.insertBefore(p, WRITELINES);
+
+  /*
+  if input is empty or a collection of spaces, 
+  just insert a prompt before #write-lines
+  */
+  if (userInput.length !== 0) {
+    console.log("Hello World!"); //please remember to remove this nathan
+  }
+
+  USERINPUT.value = resetInput;
+  userInput = resetInput; 
+}
